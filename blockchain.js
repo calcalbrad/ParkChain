64b6456fd1d64ae86f7b1354ba965300a4bec23d
Callum <callumbradding@gmail.com>
2020-10-17 16:50:36 +1300

    Updating before calling it a day

--- blockchain.js
+++ blockchain.js
@@ -2,4 +2,11 @@ const provider = Window.Provider;
 const signer = Window.Signer;
 const contract = Window.ParkChainContract;
 
+const buyerAddress = "0x70b8914813b9a681b1F1F6E057c0B781157916A3";
+const lenderAddress = "0x7767476AeaAB0bC4786c717220Af4f0C00A00D16";
 
+function parkToChainTransaction() {
+
+}
+
+parkToChainTransaction();
